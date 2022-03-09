c870ae9a9e8f49c51bf1eb27819d4b161394f8a6
SamuraiZen28 <84150236+SamuraiZen28@users.noreply.github.com>
2022-03-08 23:17:49 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+var RoleStatusEnum;
+(function (RoleStatusEnum) {
+    RoleStatusEnum["NotStarted"] = "Not Started";
+    RoleStatusEnum["InProgress"] = "In Progress";
+    RoleStatusEnum["Done"] = "Done";
+})(RoleStatusEnum || (RoleStatusEnum = {}));
+var notStartedStatus = RoleStatusEnum.NotStarted;
+//................
 
